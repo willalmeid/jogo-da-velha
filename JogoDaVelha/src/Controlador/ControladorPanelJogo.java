1365f54218e660129102d4d728eb6c9b6d21c8ea
Ian Alves Bezerra <IanBezerra.IA@gmail.com>
2024-10-17 14:41:07 -0300

    Vez do botão funcionando (parcialmente))

--- JogoDaVelha/src/Controlador/ControladorPanelJogo.java
+++ JogoDaVelha/src/Controlador/ControladorPanelJogo.java
@@ -8,8 +8,11 @@ import Visual.PanelJogo;
 public class ControladorPanelJogo implements ActionListener {
 	
 	PanelJogo panelJogo;
+	int vez;
+	String simbolo;
 	
 	public ControladorPanelJogo(PanelJogo panelJogo) {
+		this.vez = 0;
 		this.panelJogo = panelJogo;
 		addEventos();
 	}
@@ -27,30 +30,30 @@ public class ControladorPanelJogo implements ActionListener {
 	}
 	
 	public void actionPerformed(ActionEvent e) {
-//		String simbolo = (vez % 2 == 0) ? "X" : "O";
+    	this.simbolo = (this.vez % 2 == 0) ? "X" : "O";
 		if(e.getSource() == panelJogo.getButtonTopLeft()) {
-//			panelJogo.getButtonTopLeft().setText(simbolo);
-			panelJogo.getButtonTopLeft().setText("X");
+			panelJogo.getButtonTopLeft().setText(simbolo);
+//			panelJogo.getButtonTopLeft().setText("X");
 		} else if(e.getSource() == panelJogo.getButtonTopCenter()) {
-//			panelJogo.getButtonTopCenter().setText(simbolo);
-			panelJogo.getButtonTopCenter().setText("X");
+			panelJogo.getButtonTopCenter().setText(simbolo);
+//			panelJogo.getButtonTopCenter().setText("X");
 		} else if(e.getSource() == panelJogo.getButtonTopRight()) {
-//			panelJogo.getButtonTopRight().setText(simbolo);
-			panelJogo.getButtonTopRight().setText("X");
+			panelJogo.getButtonTopRight().setText(simbolo);
+//			panelJogo.getButtonTopRight().setText("X");
 		} else if(e.getSource() == panelJogo.getButtonCenterLeft()) {
-//			panelJogo.getButtonCenterLeft().setText(simbolo);
-			panelJogo.getButtonCenterLeft().setText("X");
+			panelJogo.getButtonCenterLeft().setText(simbolo);
+//			panelJogo.getButtonCenterLeft().setText("X");
 		} else if(e.getSource() == panelJogo.getButtonCenterCenter()) {
-//			panelJogo.getButtonCenterCenter().setText(simbolo);
+			panelJogo.getButtonCenterCenter().setText(simbolo);
 		} else if(e.getSource() == panelJogo.getButtonCenterRight()) {
-//			panelJogo.getButtonCenterRight().setText(simbolo);
+			panelJogo.getButtonCenterRight().setText(simbolo);
 		} else if(e.getSource() == panelJogo.getButtonBotLeft()) {
-//			panelJogo.getButtonBotLeft().setText(simbolo);
+			panelJogo.getButtonBotLeft().setText(simbolo);
 		} else if(e.getSource() == panelJogo.getButtonBotCenter()) {
-//			panelJogo.getButtonBotCenter().setText(simbolo);
+			panelJogo.getButtonBotCenter().setText(simbolo);
 		} else if(e.getSource() == panelJogo.getButtonBotRight()) {
-//			panelJogo.getButtonBotRigth().setText(simbolo);
+			panelJogo.getButtonBotRight().setText(simbolo);
 		}
-//      vez++;
+        this.vez++;
 	}
 }
